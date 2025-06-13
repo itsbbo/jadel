f3a3b8d9185ead873ebccb049277402eef1ced27
samuelsih <samuel.205025@mhs.its.ac.id>
2025-06-13 16:22:52 +0700

    feat: dashboard ui

---
+++ ui/js/types/index.d.ts
@@ -0,0 +1,39 @@
+import { LucideIcon } from 'lucide-react';
+
+export interface Auth {
+    user: User;
+}
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
+
+export interface NavGroup {
+    title: string;
+    items: NavItem[];
+}
+
+export interface NavItem {
+    title: string;
+    href: string;
+    icon?: LucideIcon | null;
+    isActive?: boolean;
+}
+
+export interface SharedData {
+    name: string;
+    quote: { message: string; author: string };
+    auth: Auth;
+    sidebarOpen: boolean;
+    [key: string]: unknown;
+}
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    avatar?: string;
+    created_at: string;
+    updated_at: string;
+}
